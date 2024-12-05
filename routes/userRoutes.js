908c7723fd08d4074db2a18a8260195715a356ea
Ashes Das <dasashes713@gmail.com>
2024-12-05 07:58:04 +0530

    "Added unfollowUser function to userRoutes.js and updated import and route accordingly"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUsers, createUser, getUserBymail, updateUser, followUser } from '../controllar/userControllar.js';
+import { getAllUsers, createUser, getUserBymail, updateUser, followUser, unfollowUser, unfollowUser } from '../controllar/userControllar.js';
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.get('/', getAllUsers);
 router.post('/mail', getUserBymail);
 router.post('/', createUser);
 router.post('/follow', followUser);
-router.post('/unfollow', followUser);
+router.post('/unfollow', unfollowUser);
 router.patch('/update',updateUser);
 
 
